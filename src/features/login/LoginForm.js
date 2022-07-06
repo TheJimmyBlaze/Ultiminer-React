8c4f76047781b403f1499fee7942a7ad04b0e2b7
Cameron Taylor <TheJImmyBlaze@gmail.com>
2022-07-07 01:26:47 +1200

    Fixing some resizing

--- src/features/login/LoginForm.js
+++ src/features/login/LoginForm.js
@@ -6,13 +6,13 @@ const LoginForm = () => {
         <Card className="p-4 position-relative shadow-dark">
             <Card.Body>
 
-                <h4 className="text-light">Login</h4>
+                <h2 className="text-light">Login</h2>
 
-                <p className="text-muted">
+                <p className="text-muted fs-5">
                     Click rocks, level up, compete.
                 </p>
 
-                <Button className="btn-discord w-100">
+                <Button className="btn-discord w-100 mt-4 fs-5">
                     <i className="bi bi-discord" /> <b>Login with Discord</b>
                 </Button>
             </Card.Body>
--- src/features/login/LoginSplash.js
+++ src/features/login/LoginSplash.js
@@ -1,14 +1,18 @@
 import LoginForm from './LoginForm';
 
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const LoginSplash = () => {
 
     return (
         <div className="d-flex flex-grow-1 w-100 bg-lava-cave">
 
-            <Container className="d-flex w-100 align-items-center justify-content-end">
-                <LoginForm/>
+            <Container className="d-flex w-100 align-items-center">
+                <Row className="d-flex w-100 justify-content-end">
+                    <Col xs="12" lg="4">
+                        <LoginForm/>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
