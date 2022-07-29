9246d9e645252d0debaea47d1e51b0f8b9c99ec4
Cameron Taylor <TheJImmyBlaze@gmail.com>
2022-07-29 22:35:25 +1200

    Defuse with progress bar

--- src/features/common/TimeDefusedButton.js
+++ src/features/common/TimeDefusedButton.js
@@ -1,28 +1,82 @@
-import { useEffect, memo } from 'react';
+import { useEffect, useMemo, memo, useState } from 'react';
+import { Button, ProgressBar } from 'react-bootstrap';
 import moment from 'moment';
 
 const TimeDefusedButton = memo(({
-   frameRate = 100,
-   defusedAt
+    frameRate = 100,
+    defusedAt,
+    onClick,
+    variant,
+    className,
+    children
 }) => {
 
-    const [defusedInSeconds, setDefusedInSeconds] = useState(0);
-    const []
+    const [totalDefuseTime, setTotalDefuseTime] = useState(moment.duration(0));
+    const [defusedIn, setDefusedIn] = useState(moment.duration(0));
 
-    const isDefused = memo(() => moment() > defusedAt);
+    const isDefused = useMemo(() => moment() >= moment(defusedAt), [defusedIn, defusedAt]);
+
+    const countDown = useMemo(() => `${defusedIn.seconds()}.${parseInt(defusedIn.milliseconds() / 100, 10)}`, [defusedIn]);
+    
+    const percentRemaining = useMemo(() => {
+
+        if (isDefused) {
+            return 100;
+        }
+
+        const remainingDefuseTime = defusedIn.asMilliseconds();
+        const percentage = remainingDefuseTime / totalDefuseTime;
+
+        const remaining = 100 - 100 * percentage;
+        return remaining;
+
+    }, [defusedIn]);
+
+    useEffect(() => {
+        setTotalDefuseTime(moment.duration(moment(defusedAt).diff(moment())).asMilliseconds());
+    }, [defusedAt]);
 
     useEffect(() => {
 
-        const render = setInterval(() => {
+        const render = setTimeout(() => {
+
+            if (!isDefused) {
+                setDefusedIn(moment.duration(moment(defusedAt).diff(moment())));
+            }
 
         }, frameRate);
 
-        return clearInterval(render);
+        return () => clearTimeout(render);
 
-    }, [props.defusedAt]);
+    }, [defusedAt, defusedIn]);
 
     return (
+        <Button variant={variant}
+            className={className}
+            onClick={onClick}
+            disabled={!isDefused}>
+            
+                <div className="position-relative h-100 w-100">
 
+                    <ProgressBar variant={variant}
+                        now={percentRemaining}
+                        className="h-100 w-100"
+                        />
+                    
+                    <div className="position-absolute top-0 bottom-0 start-0 end-0 d-flex align-items-center justify-content-center">
+                        {
+                            <>
+                                <div hidden={isDefused}>
+                                    { countDown }
+                                </div>
+                                <div hidden={!isDefused}>
+                                    { children }
+                                </div>
+                            </>
+                        }
+                    </div>
+                </div>
+        </Button>
     )
 });
 
--- src/features/mining/MiningButtons.js
+++ src/features/mining/MiningButtons.js
@@ -1,61 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Button, ProgressBar } from 'react-bootstrap';
+import moment from 'moment';
+import TimeDefusedButton from '../common/TimeDefusedButton';
 
 const MiningButtons = () => {
 
-    const [progress, setProgress] = useState(0);
+    const [backOffUntil, setBackOffUntil] = useState();
 
-    useEffect(() => {
-
-        const updateProgress = setInterval(() => {
-            if (progress > 0) {
-                setProgress(progress -1);
-            }
-        }, 100);
-
-        return () => clearInterval(updateProgress);
-    }, [progress]);
+    const pushBackOff = () => {
+        setBackOffUntil(moment().add(3, "seconds"));
+    }
 
     return (
         <div className="m-3 d-flex">
-            <Button variant="primary"
-                className="mine-button w-100 p-0"
-                onClick={() => setProgress(100)}
-                disabled={progress !== 0}>
-                
-                    <div className="position-relative h-100 w-100">
 
-                        <ProgressBar variant="primary" 
-                            now={100 - progress} 
-                            className="h-100 w-100"
-                            disabled="false"
-                            />
-                        
-                        <div className="position-absolute top-0 bottom-0 start-0 end-0 d-flex align-items-center justify-content-center">
-                            {
-                                <>
-                                    <div hidden={!progress}>
-                                        {progress}
-                                    </div>
-                                    <div hidden={progress}>
-                                        <i class="fa-solid fa-bolt" /> Mine
-                                    </div>
-                                </>
-                            }
-                        </div>
-                    </div>
-            </Button>
+            <TimeDefusedButton variant="primary"
+                className="mine-button w-100 p-0"
+                onClick={() => pushBackOff()}
+                defusedAt={backOffUntil}
+                >
+
+                <i className="fa-solid fa-bolt" /> Mine
+            </TimeDefusedButton>
 
             <div className="d-flex w-100 justify-content-end">
                 <Button variant="secondary"
                     className="other-button w-100 mx-2"
                     disabled>
-                    <i class="fa-solid fa-dollar-sign" /> Sell
+                    <i className="fa-solid fa-dollar-sign" /> Sell
                 </Button>
                 <Button variant="secondary"
                     className="other-button w-100"
                     disabled>
-                    <i class="fa-solid fa-tag" /> Shop
+                    <i className="fa-solid fa-tag" /> Shop
                 </Button>
             </div>
         </div>
